969ec45041c117313a69c01d49edcc971d46d4b8
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-04-08 16:38:08 +0200

    Visual editor uses actual publications data

--- modules/visualEditor.js
+++ modules/visualEditor.js
@@ -1,11 +1,15 @@
 rng.modules.visualEditor = function(sandbox) {
 
+    var data = sandbox.getBootstrappedData();
+    var view = $(sandbox.getTemplate('main')({title: data.title, content: data.text}));
+    
+
     return {
         start: function() {
             sandbox.publish('ready');
         },
         getView: function() {
-            return $('<p>visual editor</p>');
+            return view;
         }
     
     }
