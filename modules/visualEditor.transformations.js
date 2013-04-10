56ab6eadd6887a63a83c768abf3c02393d1f4a28
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-04-10 15:32:39 +0200

    editor: extracting metadata from xml

--- modules/visualEditor.transformations.js
+++ modules/visualEditor.transformations.js
@@ -1,23 +1,42 @@
-rng.modules.visualEditor.transformations = {};
+if(typeof module !== 'undefined' && module.exports) {
+    var $ = require('jquery');
+}
 
-rng.modules.visualEditor.transformations.fromXML = {
-    getHTMLTree: function(xml) {
-        return xml;
-    },
-    getMetaData: function(xml) {
-        return {};
-    },
-    getDocumentDescription: function(xml) {
-        return {
-            HTMLTree: this.getHTMLTree(xml),
-            metadata: this.getMetaData(xml)
+(function($) {
+
+    var transformations = {};
+
+    transformations.fromXML = {
+        getHTMLTree: function(xml) {
+            return xml;
+        },
+        getMetaData: function(xml) {
+            var toret = {};
+            $(xml).find('metadata').children().each(function() {
+                var node = $(this);
+                toret[this.nodeName.split(':')[1].toLowerCase()] = node.text();
+            })
+            return toret;
+        },
+        getDocumentDescription: function(xml) {
+            return {
+                HTMLTree: this.getHTMLTree(xml),
+                metadata: this.getMetaData(xml)
+            }
         }
     }
 
-}
-
-rng.modules.visualEditor.transformations.toXML = {
-    getXML: function(documentDescription) {
-        return documentDescription.HTMLTree;
+    transformations.toXML = {
+        getXML: function(documentDescription) {
+            return documentDescription.HTMLTree;
+        }
     }
-}
+
+
+    if(typeof module !== 'undefined' && module.exports) {
+        module.exports = transformations;
+    } else {
+        rng.modules.visualEditor.transformations = transformations;
+    }
+
+})($);
