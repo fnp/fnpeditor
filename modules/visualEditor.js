9385033bf213d7d00f0646c1ee3b3aa8a463cf02
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-04-15 15:42:04 +0200

    Separate metadata ui in visual editor

--- modules/visualEditor.js
+++ modules/visualEditor.js
@@ -4,8 +4,31 @@ rng.modules.visualEditor = function(sandbox) {
     var view = $(sandbox.getTemplate('main')());
     var isDirty = false;
     
+    var getMetaData = function() {
+        var toret = {};
+        view.find('#rng-visualEditor-meta table tr').each(function() {
+            var tr = $(this);
+            var key = $(tr.find('td')[0]).html();
+            var value = $(tr.find('td input')[0]).val();
+            toret[key] = value;
+        });
+        console.log(toret);
+        return toret;
+    };
+    
+    var setMetaData = function(metadata) {
+        var table = view.find('#rng-visualEditor-meta table');
+        table.empty();
+        _.each(_.keys(metadata), function(key) {    
+            $(sandbox.getTemplate('metaItem')({key: key, value: metadata[key]})).appendTo(table);
+        });
+    };
     
     $('#rng-visualEditor-content', view).on('keyup', function() {
+        isDirty = true;
+    });
+    
+    $('#rng-visualEditor-meta', view).on('keyup', function() {
         isDirty = true;
     });
     
@@ -19,10 +42,11 @@ rng.modules.visualEditor = function(sandbox) {
         setDocument: function(xml) {
             var transformed = transformations.fromXML.getDocumentDescription(xml);
             $('#rng-visualEditor-content', view).html(transformed.HTMLTree);
+            setMetaData(transformed.metadata);
             isDirty = false;
         },
         getDocument: function() {
-            return transformations.toXML.getXML({HTMLTree: $('#rng-visualEditor-content').text(), metadata: {}});
+            return transformations.toXML.getXML({HTMLTree: $('#rng-visualEditor-content').html(), metadata: getMetaData()});
         },
         isDirty: function() {
             return isDirty;
