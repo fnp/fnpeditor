b7266f76b76a0a67000144d2780a8f64967fafb4
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-06-18 12:07:51 +0200

    Layout informs its views on being shown

--- fnpjs/layout.js
+++ fnpjs/layout.js
@@ -2,12 +2,25 @@ define(['libs/jquery-1.9.1.min', 'libs/underscore-min'], function($ ,_) {
     'use strict';
       
     var Layout = function(template) {
+        var layout = this;
         this.dom = $(_.template(template)());
+        this.views = {};
+        
+        this.dom.onShow = function() {
+            _.values(layout.views).forEach(function(view) {
+                if(view.onShow)
+                    view.onShow();
+            });
+        };
         
     };
     
     Layout.prototype.setView = function(place, view) {
         this.dom.find('[fnpjs-place=' + place + ']').append(view);
+        this.views[place] = view;
+        if(this.dom.is(':visible') && view.onShow) {
+            view.onShow();
+        }
     };
     
     Layout.prototype.getAsView = function() {
