0ef810e2c7842de6932cc8a6c95ad5fd3ddb7cea
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-10-03 16:31:52 +0200

    getOtherAttributes

--- src/wlxml/wlxml.test.js
+++ src/wlxml/wlxml.test.js
@@ -24,6 +24,11 @@ describe('WLXMLDocument', function() {
             var node = nodeFromXML('<span meta-attr1="val1" meta-attr2="val2"></span>');
             expect(node.getMetaAttributes()).to.eql({attr1: 'val1', attr2: 'val2'});
         });
+
+        it('returns attributes other than class and meta-* as other attributes', function() {
+            var node = nodeFromXML('<span class="uri" meta-attr="val" attr1="val1" attr2="val2"></span>');
+            expect(node.getOtherAttributes()).to.eql({attr1: 'val1', attr2: 'val2'});
+        });
     });
 
 });
