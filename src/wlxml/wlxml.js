915c66cb6b6db5a535802f5981d2d9575dd608b5
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-10-24 09:58:14 +0200

    smartxml: normalizing document in loadXML instead of constructor
    To support loading new xml into the same instance of a document.

--- src/wlxml/wlxml.js
+++ src/wlxml/wlxml.js
@@ -96,87 +96,6 @@ $.extend(WLXMLElementNode.prototype, smartxml.ElementNode.prototype, {
 
 var WLXMLDocument = function(xml) {
     smartxml.Document.call(this, xml);
-
-    $(this.dom).find(':not(iframe)').addBack().contents()
-    .filter(function() {return this.nodeType === Node.TEXT_NODE;})
-    .each(function() {
-        var el = $(this),
-            text = {original: el.text(), trimmed: $.trim(el.text())},
-            elParent = el.parent(),
-            hasSpanParent = elParent.prop('tagName') === 'SPAN',
-            hasSpanBefore = el.prev().length && $(el.prev()).prop('tagName') === 'SPAN',
-            hasSpanAfter = el.next().length && $(el.next()).prop('tagName') === 'SPAN';
-
-
-        var addInfo = function(toAdd, where, transformed, original) {
-            var parentContents = elParent.contents(),
-                idx = parentContents.index(el[0]),
-                prev = idx > 0 ? parentContents[idx-1] : null,
-                next = idx < parentContents.length - 1 ? parentContents[idx+1] : null,
-                target, key;
-
-            if(where === 'above') {
-                target = prev ? $(prev) : elParent;
-                key = prev ? 'orig_after' : 'orig_begin';
-            } else if(where === 'below') {
-                target = next ? $(next) : elParent;
-                key = next ? 'orig_before' : 'orig_end';
-            } else { throw new Object;}
-
-            target.data(formatter_prefix + key, toAdd);
-            if(transformed !== undefined) {
-                target.data(formatter_prefix + key + '_transformed', transformed);
-            }
-            if(original !== undefined) {
-                target.data(formatter_prefix + key + '_original', original);   
-            }
-        }
-
-        text.transformed = text.trimmed;
-
-        if(hasSpanParent || hasSpanBefore || hasSpanAfter) {
-            var startSpace = /\s/g.test(text.original.substr(0,1)),
-                endSpace = /\s/g.test(text.original.substr(-1)) && text.original.length > 1;
-            text.transformed = (startSpace && (hasSpanParent || hasSpanBefore) ? ' ' : '');
-            text.transformed += text.trimmed;
-            text.transformed += (endSpace && (hasSpanParent || hasSpanAfter) ? ' ' : '');
-        } else {
-            if(text.trimmed.length === 0 && text.original.length > 0 && elParent.contents().length === 1) {
-                text.transformed = ' ';
-            }
-        }
-
-        if(!text.transformed) {
-            addInfo(text.original, 'below');
-            el.remove();
-            return true; // continue
-        }
-
-        if(text.transformed !== text.original) {
-            // if(!text.trimmed) {
-            //     addInfo(text.original, 'below');
-            // } else {
-                var startingMatch = text.original.match(/^\s+/g),
-                    endingMatch = text.original.match(/\s+$/g),
-                    startingWhiteSpace = startingMatch ? startingMatch[0] : null,
-                    endingWhiteSpace = endingMatch ? endingMatch[0] : null;
-
-                if(endingWhiteSpace) {
-                    if(text.transformed[text.transformed.length - 1] === ' ' && endingWhiteSpace[0] === ' ')
-                        endingWhiteSpace = endingWhiteSpace.substr(1);
-                    addInfo(endingWhiteSpace, 'below', !text.trimmed ? text.transformed : undefined, !text.trimmed ? text.original : undefined);
-                }
-
-                if(startingWhiteSpace && text.trimmed) {
-                    if(text.transformed[0] === ' ' && startingWhiteSpace[startingWhiteSpace.length-1] === ' ')
-                        startingWhiteSpace = startingWhiteSpace.substr(0, startingWhiteSpace.length -1);
-                    addInfo(startingWhiteSpace, 'above', !text.trimmed ? text.transformed : undefined, !text.trimmed ? text.original : undefined);
-                }
-            //}
-        }
-
-        el.replaceWith(document.createTextNode(text.transformed));
-    });
 };
 
 var formatter_prefix = '_wlxml_formatter_';
@@ -184,6 +103,92 @@ var formatter_prefix = '_wlxml_formatter_';
 WLXMLDocument.prototype = Object.create(smartxml.Document.prototype);
 $.extend(WLXMLDocument.prototype, {
     ElementNodeFactory: WLXMLElementNode,
+
+    loadXML: function(xml) {
+        smartxml.Document.prototype.loadXML.call(this, xml);
+        $(this.dom).find(':not(iframe)').addBack().contents()
+            .filter(function() {return this.nodeType === Node.TEXT_NODE;})
+            .each(function() {
+                var el = $(this),
+                    text = {original: el.text(), trimmed: $.trim(el.text())},
+                    elParent = el.parent(),
+                    hasSpanParent = elParent.prop('tagName') === 'SPAN',
+                    hasSpanBefore = el.prev().length && $(el.prev()).prop('tagName') === 'SPAN',
+                    hasSpanAfter = el.next().length && $(el.next()).prop('tagName') === 'SPAN';
+
+
+                var addInfo = function(toAdd, where, transformed, original) {
+                    var parentContents = elParent.contents(),
+                        idx = parentContents.index(el[0]),
+                        prev = idx > 0 ? parentContents[idx-1] : null,
+                        next = idx < parentContents.length - 1 ? parentContents[idx+1] : null,
+                        target, key;
+
+                    if(where === 'above') {
+                        target = prev ? $(prev) : elParent;
+                        key = prev ? 'orig_after' : 'orig_begin';
+                    } else if(where === 'below') {
+                        target = next ? $(next) : elParent;
+                        key = next ? 'orig_before' : 'orig_end';
+                    } else { throw new Object;}
+
+                    target.data(formatter_prefix + key, toAdd);
+                    if(transformed !== undefined) {
+                        target.data(formatter_prefix + key + '_transformed', transformed);
+                    }
+                    if(original !== undefined) {
+                        target.data(formatter_prefix + key + '_original', original);
+                    }
+                }
+
+                text.transformed = text.trimmed;
+
+                if(hasSpanParent || hasSpanBefore || hasSpanAfter) {
+                    var startSpace = /\s/g.test(text.original.substr(0,1)),
+                        endSpace = /\s/g.test(text.original.substr(-1)) && text.original.length > 1;
+                    text.transformed = (startSpace && (hasSpanParent || hasSpanBefore) ? ' ' : '');
+                    text.transformed += text.trimmed;
+                    text.transformed += (endSpace && (hasSpanParent || hasSpanAfter) ? ' ' : '');
+                } else {
+                    if(text.trimmed.length === 0 && text.original.length > 0 && elParent.contents().length === 1) {
+                        text.transformed = ' ';
+                    }
+                }
+
+                if(!text.transformed) {
+                    addInfo(text.original, 'below');
+                    el.remove();
+                    return true; // continue
+                }
+
+                if(text.transformed !== text.original) {
+                    // if(!text.trimmed) {
+                    //     addInfo(text.original, 'below');
+                    // } else {
+                        var startingMatch = text.original.match(/^\s+/g),
+                            endingMatch = text.original.match(/\s+$/g),
+                            startingWhiteSpace = startingMatch ? startingMatch[0] : null,
+                            endingWhiteSpace = endingMatch ? endingMatch[0] : null;
+
+                        if(endingWhiteSpace) {
+                            if(text.transformed[text.transformed.length - 1] === ' ' && endingWhiteSpace[0] === ' ')
+                                endingWhiteSpace = endingWhiteSpace.substr(1);
+                            addInfo(endingWhiteSpace, 'below', !text.trimmed ? text.transformed : undefined, !text.trimmed ? text.original : undefined);
+                        }
+
+                        if(startingWhiteSpace && text.trimmed) {
+                            if(text.transformed[0] === ' ' && startingWhiteSpace[startingWhiteSpace.length-1] === ' ')
+                                startingWhiteSpace = startingWhiteSpace.substr(0, startingWhiteSpace.length -1);
+                            addInfo(startingWhiteSpace, 'above', !text.trimmed ? text.transformed : undefined, !text.trimmed ? text.original : undefined);
+                        }
+                    //}
+                }
+
+                el.replaceWith(document.createTextNode(text.transformed));
+            });
+        this.trigger('contentSet');
+    }
+
 });
 
 
