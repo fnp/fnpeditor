25175ece1135d9757fb1e5782c20d12ba804a0d4
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-12-30 13:16:05 +0100

    logging: Raven handler
    This handler assumes that ravenjs is already installed in a global
    scope.

--- src/fnpjs/logging/handlers.js
+++ src/fnpjs/logging/handlers.js
@@ -3,6 +3,26 @@ define(function() {
 'use strict';
 
 
-return {};
+return {
+    raven: function(msg, level, data) {
+        /* global window */
+        if(!window.Raven) {
+            return;
+        }
+
+        var ravenData = {};
+
+        if(data.exception) {
+            window.Raven.captureException(data.exception);
+        } else {
+            Object.keys(data || {}).forEach(function(key) {
+                ravenData[key] = data[key];
+            });
+            ravenData.tags = ravenData.tags || {};
+            ravenData.tags.level = level;
+            window.Raven.captureMessage(msg, ravenData);
+        }
+    }
+};
 
 });
