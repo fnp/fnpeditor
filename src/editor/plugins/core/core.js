aba92db36cedd657b0c8c0637aa4aae7b573f023
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2014-06-26 11:40:44 +0200

    editor: fix splitting text when its parent contains comments

--- src/editor/plugins/core/core.js
+++ src/editor/plugins/core/core.js
@@ -18,22 +18,27 @@ plugin.documentExtension.textNode.transformations = {
     breakContent: {
         impl: function(args) {
             var node = this,
+                parentDescribingNodes = [],
                 newNodes, emptyText;
             newNodes = node.split({offset: args.offset});
-            [newNodes.first, newNodes.second].some(function(newNode) {
-                if(!(newNode.contents().length)) {
-                    emptyText = newNode.append({text: ''});
-                    return true; // break
-                }
-            });
             newNodes.second.contents()
                 .filter(function(child) {
                     return child.object.describesParent;
                 })
                 .forEach(function(child) {
                     //child.detach();
-                    newNodes.first.append(child);
+                    parentDescribingNodes.push(child);
+                    child.detach();
                 });
+            [newNodes.first, newNodes.second].some(function(newNode) {
+                if(!(newNode.contents().length)) {
+                    emptyText = newNode.append({text: ''});
+                    return true; // break
+                }
+            });
+            parentDescribingNodes.forEach(function(node) {
+                newNodes.first.append(node);
+            });
             return _.extend(newNodes, {emptyText: emptyText});
         },
         getChangeRoot: function() {
